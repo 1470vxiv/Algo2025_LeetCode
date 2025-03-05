7205191648bab78364d32d9b2becaaf2f9f5dae5
1470vxiv <138235011+1470vxiv@users.noreply.github.com>
2025-03-05 17:45:54 +0800

    Add files via upload

---
+++ Week1/239.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+class Solution {
+
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> result;
+        deque<int> dq;
+        int m = nums.size();
+        int max_idx = 0;
+
+        for(int i=0; i < m; i++){
+            
+            if (!dq.empty() && dq.front() == i - k) {
+                dq.pop_front();
+            }
+
+            // Remove elements from the back that are smaller than the current element
+            while (!dq.empty() && nums[dq.back()] < nums[i]) {
+                dq.pop_back();
+            }
+
+
+            // Add current element index
+            dq.push_back(i);     
+            
+
+            if(i >= k-1) result.push_back(nums[dq.front()]);
+        }
+
+    return result;   
+        
+    }
+};
+
+int main()
+{
+    vector<int> nums = {1,3,-1,-3,5,3,6,7};
+    int k = 3;
+    Solution sol;
+    vector<int> result = sol.maxSlidingWindow(nums, k);
+
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+
+}

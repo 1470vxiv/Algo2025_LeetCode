e04deef254485eae95a69503d0159e8a575fcde4
1470vxiv <138235011+1470vxiv@users.noreply.github.com>
2025-03-20 14:54:22 +0800

    Update 45_Jump_game_II.cpp

--- Week2/45_Jump_game_II.cpp
+++ Week2/45_Jump_game_II.cpp
@@ -8,17 +8,17 @@ class Solution {
             int result = 0;
             int m = nums.size();
             int nowIdx = 0;
-            int farthest = 0;
+            int farthest = 0; // 從index i出發，能抵達的最遠位置
 
             if(m == 1) return 0;
             
             for(int i=0; i<m-1; i++)
             {
-                farthest = max(farthest, i+nums[i]);
+                farthest = max(farthest, i+nums[i]); // 每個i都要更新farthest
 
                 if(i == nowIdx){
                     result++;
-                    nowIdx =  farthest;
+                    nowIdx =  farthest; //i輪到nowIdx時才執行，一次就移動到farthest
                 }
             }
         

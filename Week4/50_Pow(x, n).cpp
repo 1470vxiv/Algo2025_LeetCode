7a562431047af9522ee90d04cbb9556a2bd31253
1470vxiv <138235011+1470vxiv@users.noreply.github.com>
2025-03-21 13:38:20 +0800

    Update 50_Pow(x, n).cpp

--- Week4/50_Pow(x, n).cpp
+++ Week4/50_Pow(x, n).cpp
@@ -1,24 +1,30 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 class Solution {
     public:
         double myPow(double x, int n) {
+            bool a=false;
             
+
+            if (n == 0) return 1.0;//終止條件
+            if(n == -2147483648)
+            {
+                a = true;
+                x = 1/x;
+                n = 2147483647;
+            }
+
+            else if (n < 0)
+            {
+                x = 1/x;
+                n = -n;
+            }
+            
+            double half = myPow(x,n/2);
+            if(n == 2147483647 && a == true) return x * x * half * half;
+            else if(n%2 == 0) return half * half;
+            else return x * half * half;
         }
     };
 
-int main()
-{
-    double x = 2;
-    int n = 10;
-    Solution sol;
-    double result = sol.myPow(x,n);
-
-    
-    cout <<result << endl;
-
-    return 0;
-
-}

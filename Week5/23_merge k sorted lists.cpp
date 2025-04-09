9197ed86558b505974f0411fd3127b714ec38e08
1470vxiv <138235011+1470vxiv@users.noreply.github.com>
2025-04-09 17:15:26 +0800

    Add files via upload

---
+++ Week5/23_merge k sorted lists.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//Definition of single-ended link list
+struct ListNode {
+         int val;
+         ListNode* next;
+         ListNode() : val(0), next(nullptr) {}
+         ListNode(int x) : val(x), next(nullptr) {}
+         ListNode(int x, ListNode* next) : val(x), next(next) {}
+     };
+
+class Solution {
+
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        lists.erase(remove(lists.begin(), lists.end(), nullptr), lists.end());
+        if(lists.empty()) return nullptr;
+
+        int minVal = 10000;
+        int minIndex = 0;
+        for(int i=0; i<lists.size(); i++)
+        {
+            if(lists[i] == nullptr){
+                lists.erase(lists.begin()+i);   
+            }
+            else if(lists[i]->val < minVal){
+                minVal = lists[i]->val;
+                minIndex = i;
+            }
+        }
+        if(lists.empty()) return nullptr;
+        
+        ListNode* head = lists[minIndex];
+        lists[minIndex] = lists[minIndex]->next;
+        if(lists[minIndex] == nullptr) lists.erase(lists.begin()+minIndex);
+        head->next = mergeKLists(lists);
+        return head;
+    
+        
+    }
+};

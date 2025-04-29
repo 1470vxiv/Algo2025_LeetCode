1b9d5eb36c456d4156cd7bf3f7a7ebec5fe99e25
1470vxiv <138235011+1470vxiv@users.noreply.github.com>
2025-04-29 13:21:45 +0800

    Update 815. Bus Routes.cpp

--- Week7/815. Bus Routes.cpp
+++ Week7/815. Bus Routes.cpp
@@ -3,7 +3,7 @@ public:
     int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
         if (source == target) return 0;
 
-        // Map each stop to the buses (routes) that go through it
+        // Map each stop to the buses that go through it
         unordered_map<int, vector<int>> stopToBuses;
         for (int i = 0; i < routes.size(); i++) {
             for (int stop : routes[i]) {
@@ -18,26 +18,27 @@ public:
         q.push(source);
         visitedStops.insert(source);
 
-        int busesTaken = 0;
+        int busesTaken = 0; //result
 
         while (!q.empty()) {
             int levelSize = q.size();
             busesTaken++;
 
+            //BFS
             for (int i = 0; i < levelSize; i++) {
-                int currStop = q.front();
+                int currStop = q.front(); //本層從currStop出發
                 q.pop();
 
                 for (int bus : stopToBuses[currStop]) {
                     if (visitedBuses.count(bus)) continue;
 
-                    visitedBuses.insert(bus);
+                    visitedBuses.insert(bus); //造訪currStop對應的bus
 
                     for (int nextStop : routes[bus]) {
-                        if (nextStop == target) return busesTaken;
-                        if (!visitedStops.count(nextStop)) {
+                        if (nextStop == target) return busesTaken; //hit
+                        if (!visitedStops.count(nextStop)) { //若nextStop未被造訪
                             visitedStops.insert(nextStop);
-                            q.push(nextStop);
+                            q.push(nextStop); //q插入當前bus對應的nextStop
                         }
                     }
                 }

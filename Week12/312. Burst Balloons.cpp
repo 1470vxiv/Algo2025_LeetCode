91ef3044c53f3e91bee7a56713b2523dc9e738ad
1470vxiv <138235011+1470vxiv@users.noreply.github.com>
2025-06-05 00:48:41 +0800

    Update 312. Burst Balloons.cpp

--- Week12/312. Burst Balloons.cpp
+++ Week12/312. Burst Balloons.cpp
@@ -12,12 +12,15 @@ public:
         nums.push_back(1);
 
         // Create DP table
+        //dp[left][right]: maximum coins we can get by bursting only the balloons between left and right
         vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
 
-        // Length of the subarray we are considering
+        //traversal by 間距長度
         for (int len = 2; len <= n + 1; ++len) {
+            //traversal by left位置
             for (int left = 0; left + len <= n + 1; ++left) {
                 int right = left + len;
+                //traversal by 嘗試打破氣球之index
                 for (int i = left + 1; i < right; ++i) {
                     dp[left][right] = max(dp[left][right],
                         nums[left] * nums[i] * nums[right] +
